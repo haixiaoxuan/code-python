71db87b455a7dc8e8eeba1cc0061486036bc711a
xiexiaoxuan <xiexiaoxuan@xd.com>
2021-10-12 17:53:21 +0800

    add

--- tf2.0/tf2/强化学习/乒乓球/01_dqn.py
+++ tf2.0/tf2/强化学习/乒乓球/01_dqn.py
@@ -34,19 +34,13 @@ Action_size = 3
 
 def pre_processing(observation):
     """ 预处理 """
+    # 以整数存储节省空间，取消归一化
     processed_observation = rgb2gray(observation) * 255
-    # Convering to grey converts it to normalised values [0,255] -> [0,1]
-    # We need to store the values as integers in the next step to save space, so we need to undo the normalisation
     processed_observation = resize(processed_observation, (84, 84), mode="constant")
-    # Convert from floating point to integers ranging from [0,255]
     processed_observation = np.uint8(processed_observation)
     return processed_observation
 
 
-# Define the huber loss function
-# Used for the Keras model compile function
-# Needs to take y and yhat and output loss
-# Needs investigation
 def huber_loss(y, q_value):
     error = tf.abs(y - q_value)
     quadratic_part = tf.clip_by_value(error, 0.0, 1.0)
@@ -55,79 +49,47 @@ def huber_loss(y, q_value):
     return loss
 
 
-# Create the model to approximate Qvalues for a given set of states and actions
 def atari_model():
-    # Define the inputs
     frames_input = keras.Input(shape=Input_shape, name="frames")
-    actions_input = keras.layers.Input(shape=(Action_size,), name="action_mask")
+    actions_input = keras.layers.Input(shape=(Action_size,), name="action_mask")        # (1,1,1), (1,0,0)
 
-    # Normalise the inputs from [0,255] to [0,1] - to make processing easier
     normalised = keras.layers.Lambda(lambda x: x / 255.0, name="normalised")(frames_input)
-    # Conv1 is 16 8x8 filters with a stride of 4 and a ReLU
     conv1 = keras.layers.Conv2D(16, 8, 4, activation="relu")(normalised)
-    # Conv2 is 32 4x4 filters with a stride of 2 and a ReLU
     conv2 = keras.layers.Conv2D(32, 4, 2, activation="relu")(conv1)
-    # Flatten the output from Conv2
     conv2_flatten = keras.layers.Flatten()(conv2)
-    # Then a fully connected layer with 256 ReLU units
     dense1 = keras.layers.Dense(256, activation="relu")(conv2_flatten)
-    # Then a fully connected layer with a unit to map to each of the actions and no activation
     output = keras.layers.Dense(Action_size)(dense1)
     # Then we multiply the output by the action mask
     # When trying to find the value of all the actions this will be a mask full of 1s
     # When trying to find the value of a specific action, the mask will only be 1 for a single action
     filtered_output = keras.layers.Multiply(name="Qvalue")([output, actions_input])
 
-    # Create the model
-    # Create a model that maps frames and actions to the filtered output
     model = keras.Model(inputs=[frames_input, actions_input], outputs=filtered_output)
-    # Print a summary of the model
     model.summary()
-    # Define optimiser
     optimiser = keras.optimizers.Adam()
-    # Compile model
     model.compile(optimizer=optimiser, loss=huber_loss)
-    # Return the model
     return model
 
 
-# Create an identical model to use as a target
 def atari_model_target():
-    # Define the inputs
     frames_input = keras.Input(shape=Input_shape, name="frames")
     actions_input = keras.layers.Input(shape=(Action_size,), name="action_mask")
 
-    # Normalise the inputs from [0,255] to [0,1] - to make processing easier
     normalised = keras.layers.Lambda(lambda x: x / 255.0, name="normalised")(frames_input)
-    # Conv1 is 16 8x8 filters with a stride of 4 and a ReLU
     conv1 = keras.layers.Conv2D(16, 8, 4, activation="relu")(normalised)
-    # Conv2 is 32 4x4 filters with a stride of 2 and a ReLU
     conv2 = keras.layers.Conv2D(32, 4, 2, activation="relu")(conv1)
-    # Flatten the output from Conv2
     conv2_flatten = keras.layers.Flatten()(conv2)
-    # Then a fully connected layer with 256 ReLU units
     dense1 = keras.layers.Dense(256, activation="relu")(conv2_flatten)
-    # Then a fully connected layer with a unit to map to each of the actions and no activation
     output = keras.layers.Dense(Action_size)(dense1)
-    # Then we multiply the output by the action mask
-    # When trying to find the value of all the actions this will be a mask full of 1s
-    # When trying to find the value of a specific action, the mask will only be 1 for a single action
     filtered_output = keras.layers.Multiply(name="Qvalue")([output, actions_input])
 
-    # Create the model
-    # Create a model that maps frames and actions to the filtered output
     model = keras.Model(inputs=[frames_input, actions_input], outputs=filtered_output)
-    # Print a summary of the model
     model.summary()
-    # Define optimiser
     optimiser = keras.optimizers.Adam()
-    # Compile model
     model.compile(optimizer=optimiser, loss=huber_loss)
-    # Return the model
     return model
 
 
-# get action from model using epsilon-greedy policy
 def get_action(history, epsilon, step, model):
     if np.random.rand() <= epsilon or step <= params.get("observe_step_num"):
         return random.randrange(Action_size)
@@ -136,8 +98,8 @@ def get_action(history, epsilon, step, model):
         return np.argmax(q_value[0])
 
 
-# save sample <s,a,r,s"> to the replay memory
 def store_memory(memory, history, action, reward, next_history):
+    # save sample <s,a,r,s"> to the replay memory
     memory.append((history, action, reward, next_history))
 
 
@@ -145,9 +107,7 @@ def get_one_hot(targets, nb_classes):
     return np.eye(nb_classes)[np.array(targets).reshape(-1)]
 
 
-# train model by random batch
 def train_memory_batch(memory, model):
-    # Sample a minibatch
     mini_batch = random.sample(memory, params.get("batch_size"))
     # Create empty arrays to load our minibatch into
     # These objects have multiple values hence need defined shapes
@@ -195,13 +155,8 @@ def train_memory_batch(memory, model):
 
 
 def train():
-    # Define which game to play
     env = gym.make("PongDeterministic-v4")
 
-    # Create a space for our memory
-    # We will use a deque - double ended que
-    # This will result in a max size so that once all the space is filled,
-    # older entries will be removed to make room for new
     memory = deque(maxlen=params.get("replay_memory"))
 
     # Start episode counter
@@ -215,22 +170,13 @@ def train():
     # Start global step
     global_step = 0
 
-    # Define model
     model = atari_model()
-
-    # Define target model
     model_target = atari_model_target()
 
-    # Define where to store logs
     log_dir = "{}/run-{}-log".format(params.get("train_dir"), "MK10")
-    # Pass graph to TensorBoard
-
     file_writer = tf.summary.create_file_writer(log_dir)
 
-    # Start the optimisation loop
     while episode_number < params.get("n_episodes"):
-
-        # Initialisise done as false
         done = False
 
         # Initialise other values
@@ -245,34 +191,27 @@ def train():
         # This way we can get a sense of whats going on
         for _ in range(random.randint(1, params.get("no_op_steps"))):
             observation, _, _, _ = env.step(1)
+
         # At the start of the episode there are no preceding frames
         # So we just copy the initial states into a stack to make the state history
         state = pre_processing(observation)
         state_history = np.stack((state, state, state, state), axis=2)
         state_history = np.reshape([state_history], (1, 84, 84, 4))
 
-        # Perform while we still have lives
         while not done:
-            # Render the image if selected to do so
             if params.get("render"):
                 env.render()
 
-            # Select an action based on our current model
             action = get_action(state_history, epsilon, global_step, model_target)
-            # Convert action from array numbers to real numbers
             real_action = action + 1
 
-            # After we"re done observing, start scaling down epsilon
             if global_step > params.get("observe_step_num") and epsilon > params.get("final_epsilon"):
                 epsilon -= epsilon_decay
 
-            # Record output from the environment
             observation, reward, done, info = env.step(real_action)
 
-            # Process the observation
             next_state = pre_processing(observation)
             next_state = np.reshape([next_state], (1, 84, 84, 1))
-            # Update the history with the next state - also remove oldest state
             state_history_w_next = np.append(next_state, state_history[:, :, :, :3], axis=3)
 
             # Update score
